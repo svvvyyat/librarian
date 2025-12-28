2bd8c62687b4424ba98adcae2596862e7a9f4d90
svvvyyat <vitkogame228@gmail.com>
2025-12-29 00:13:22 +0200

    modified:   analyzer.py 	new file:   test_data.txt

--- analyzer.py
+++ analyzer.py
@@ -13,7 +13,7 @@ def analyze_text(text):
 
     Твоє завдання — створити метадані для пошукової системи.
     Вимоги до відповіді (JSON):
-    1. "summary": Короткий опис змісту українською мовою (2-3 речення).
+    1. "description": Короткий опис змісту українською мовою (2-3 речення).
     2. "keywords": Список ключових слів та фраз для пошуку українською.
     ВАЖЛИВО: Не обмежуй кількість тегів. Випиши ВСІ важливі терміни, імена, дати або назви, які зустрічаються в тексті і можуть бути корисні для пошуку. 
     Якщо документ складний — дай більше тегів. Якщо простий — менше.
@@ -24,7 +24,7 @@ def analyze_text(text):
     }}
     """
     try:
-        response = ollama.chat(model=model, messages=[{"role": "user", "content": prompt}])
+        response = ollama.chat(model=model, messages=[{"role": "user", "content": prompt}],format='json')
         answer = response.get("message", {}).get("content", "")
         json_answer = answer.replace("```json", "").replace("```", "").strip()
         return json.loads(json_answer)
